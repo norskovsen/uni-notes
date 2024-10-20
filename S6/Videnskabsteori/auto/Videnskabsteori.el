135f2b157db962ded29457c83c308ff3232337d3
Martin Nørskov <martin.norskov.jensen@lego.com>
2024-10-20 19:31:54 +0200

    Added notes

---
+++ S6/Videnskabsteori/auto/Videnskabsteori.el
@@ -0,0 +1,45 @@
+(TeX-add-style-hook
+ "Videnskabsteori"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org2e7f5df"
+    "sec:org5d7e7b5"
+    "sec:org54f586c"
+    "sec:orge653a80"
+    "sec:orgbef2ae0"
+    "sec:org75cf0ed"
+    "sec:orga9c2bcb"
+    "sec:org87ba4b7"
+    "sec:org927cbc0"
+    "sec:org6b7e734"
+    "sec:orgb8dcdd5"))
+ :latex)
+
